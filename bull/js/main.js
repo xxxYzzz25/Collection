84c18f4363d3a8d77bd130af3a8f18e5d0088a54
Your Name <xxxzz925@gmail.com>
2019-03-21 17:38:47 +0800

    2

--- bull/js/main.js
+++ bull/js/main.js
@@ -33,7 +33,7 @@ function loadProperties(lan) {
   jQuery.i18n.properties({
     //加载资浏览器语言对应的资源文件
     name: 'strings', //资源文件名称
-    path: './i18n/', //资源文件路径
+    path: '/bull/js/i18n/', //资源文件路径
     mode: 'map', //用Map的方式使用资源文件中的值
     language: lan,
     callback: function() {
